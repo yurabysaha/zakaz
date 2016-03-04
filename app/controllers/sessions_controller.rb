a07d2395dc49e94b1045e2a8f9a7f0ce064fee58
yurabysaha <yurabysaha@gmail.com>
2016-03-04 22:59:28 +0200

    change to flash.now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      flash[:danger] = "Неправильна пошта або пароль"
+      flash.now[:danger] = "Неправильна пошта або пароль"
       redirect_to '/'
     end
   end
